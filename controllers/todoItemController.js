33e5d28b3af62d4e0d4ac31931ac956c1a13eb53
colabebsi <m.samaae@hotmail.com>
2020-09-04 13:47:46 +0200

    Added CRUC functionality for todo items

--- controllers/todoItemController.js
+++ controllers/todoItemController.js
@@ -10,5 +10,43 @@ module.exports = {
         } catch (error) {
             res.status(400).json(error.message);
         }
+    },
+    read: async (req, res) => {
+        const id = req.params.id;
+
+        try {
+            const todoItem = await TodoItem.findById(id);
+            res.status(200).json({ todoItem });
+        } catch (error) {
+            res.status(400).json(error.message);
+        }
+    },
+    readAll: async (req, res) => {
+        try {
+            const todoItems = await TodoItem.find({});
+            res.status(200).json({ todoItems });
+        } catch (error) {
+            res.status(400).json(error.message);            
+        }
+    },
+    update: async (req, res) => {
+        const id = req.params.id;
+
+        try {
+            const todoItem = await TodoItem.findByIdAndUpdate(id, { $set: req.body });
+            res.status(200).json({ todoItem });
+        } catch (error) {
+            res.status(400).json(error.message);
+        }
+    },
+    delete: async (req, res) => {
+        const id = req.params.id;
+
+        try {
+            const todoItem = await TodoItem.findByIdAndDelete(id);
+            res.status(200).json({ todoItem });
+        } catch (error) {
+            res.status(400).json(error.message);
+        }
     }
 }
--- routes/todoItemRoutes.js
+++ routes/todoItemRoutes.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const todoItemController = require('../controllers/todoItemController');
 
 router.post('/todos', todoItemController.create);
-// router.get('/todos/:id', todoController.read);
-// router.get('/todos', todoController.readAll);
-// router.patch('/todos/:id', todoController.update);
-// router.delete('/todos/:id', todoController.delete);
+router.get('/todos/:id', todoItemController.read);
+router.get('/todos', todoItemController.readAll);
+router.patch('/todos/:id', todoItemController.update);
+router.delete('/todos/:id', todoItemController.delete);
 
 module.exports = router;
